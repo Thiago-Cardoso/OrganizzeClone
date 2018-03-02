5925371a101fe8dc4622a965107263402df0ff19
Thiago-Cardoso <trovador182@gmail.com>
2018-03-02 00:32:13 -0300

    customizacao layout login e cadastro

--- app/src/main/java/cursoandroid/cardoso/thiago/com/organizze/MainActivity.java
+++ app/src/main/java/cursoandroid/cardoso/thiago/com/organizze/MainActivity.java
@@ -1,11 +1,16 @@
 package cursoandroid.cardoso.thiago.com.organizze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+import cursoandroid.cardoso.thiago.com.organizze.activity.CadastroActivity;
+import cursoandroid.cardoso.thiago.com.organizze.activity.LoginActivity;
 
 public class MainActivity extends IntroActivity {
 
@@ -42,5 +47,22 @@ public class MainActivity extends IntroActivity {
                 .build()
         );
 
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .build()
+        );
+
+    }
+
+    public void btEntrar(View view) {
+        //inicia a activity Login
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view) {
+
+        //inicia a activity Cadastro
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
